eac3d207c9c042fbda77df5a167be31ef9e4374f
Felipe Tajima <felipebtajima05@gmail.com>
2024-10-14 15:01:45 -0300

    Team slider (PRONTO)
    Concluída a área de team, com seu slider funcionando! O site enfrenta problemas de responsividade, importante dar uma atenção assim que possível

--- Projeto Landing Page/scripts/sliders.js
+++ Projeto Landing Page/scripts/sliders.js
@@ -32,3 +32,32 @@ setInterval(next, 5000);
 
 // SLIDER CARD TEAM
 
+let TEAMtotalSlides = 2;
+let TEAMcurrentSlide = 0;
+
+document.querySelector('.team-slider').style.width = `1120px;`;
+// AQUI
+
+function TEAMback() {
+    TEAMcurrentSlide--;
+    if(TEAMcurrentSlide < 0) {
+        TEAMcurrentSlide = TEAMtotalSlides - 1;
+    }
+    TEAMupdateMargin();
+}
+
+function TEAMnext() {
+    TEAMcurrentSlide++;
+    if(TEAMcurrentSlide > (TEAMtotalSlides-1)) {
+        TEAMcurrentSlide = 0;
+    }
+    TEAMupdateMargin();
+}
+
+function TEAMupdateMargin() {
+    let TEAMsliderWidth = 1120
+    let TEAMnewMargin = (TEAMcurrentSlide * TEAMsliderWidth);
+    document.querySelector('.team-slider').style.marginLeft = `-${TEAMnewMargin}px`;
+}
+
+//setInterval(TEAMnext, 5000);
